68fe9dd437c138396e969052023af5fc9441f5ad
Danny Pifer <piferdg@gmail.com>
2018-08-27 18:57:29 -0600

    Adding vue project files along with functioning header component with logo

---
+++ bookAuthorCatalog/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Header />
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header";
+export default {
+  name: 'App',
+  components: {
+    Header
+  },
+  
+  data() {
+    return {
+
+    }
+  }
+  
+}
+</script>
+
+<style>
+#app {
+  
+}
+</style>
---
+++ bookAuthorCatalog/src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="header">
+    <img src= "../assets/logo.jpg" alt="Reads Logo">
+    <h1>Galvanize Reads</h1> 
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Header',
+  
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+.header {
+  display: flex
+}
+
+</style>
